015d0a5c4dcba67001eeaa9bca0734aab70b4d06
seouuuu <seouuuu@gmail.com>
2017-05-24 03:50:17 +0900

    Complete Layout
    yayyyy :-)

--- BSLOWBSURE/js/common.js
+++ BSLOWBSURE/js/common.js
@@ -1,5 +1,39 @@
 $(document).ready(function () {
+    'use strict';
+    $('html').niceScroll({
+		cursorcolor: "#CCC",
+		cursorwidth: 5,
+		scrollspeed: 40,
+		mousescrollstep: 40,
+		background: "#FFF",
+		cursorborder: "none",
+		autohidemode: false,
+        zindex: 99999
+    });
+});
+
+/*--Scroll easing effect--*/
+$(document).ready(function () {
+    'use strict';
+	$('a').click(function () {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 800, 'easeInOutExpo');
+		return false;
+	});
+});
+
+$(document).ready(function () {
+    'use strict';
 	$('#nav_icon').click(function () {
 		$(this).toggleClass('open');
 	});
 });
+/*---
+$(document).ready(function () {
+    'use strict';
+    var x = screen.width;
+
+    document.getElementById("work01").css.width = "500px";
+    document.getElementById("work01").css.height = "100px";
+});---*/
